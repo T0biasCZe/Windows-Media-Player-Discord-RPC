ba6360a18854b8a03deb4bc1a3840adb345377af
T0biasCZe <44525446+T0biasCZe@users.noreply.github.com>
2024-10-18 01:54:57 +0200

    Revert "Win7"

--- Discord WMP/Settings1.Designer.cs
+++ Discord WMP/Settings1.Designer.cs
@@ -12,7 +12,7 @@ namespace Discord_WMP {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.11.0.0")]
     public sealed partial class Settings1 : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings1 defaultInstance = ((Settings1)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings1())));
@@ -152,18 +152,6 @@ namespace Discord_WMP {
             }
             set {
                 this["dont_hide"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
-        public bool media_keys {
-            get {
-                return ((bool)(this["media_keys"]));
-            }
-            set {
-                this["media_keys"] = value;
             }
         }
     }
